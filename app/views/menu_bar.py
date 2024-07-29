0567731b971d73d4e32143c441874e74eb30fae7
rosen-roe <2826450573@qq.com>
2024-07-29 17:31:07 +0800

    菜单栏汉化补全

--- app/views/menu_bar.py
+++ app/views/menu_bar.py
@@ -101,33 +101,33 @@ class MenuBar(QObject):
         Returns:
             QMenu: The created "File" menu.
         """
-        file_menu = self.menu_bar.addMenu("File")
+        file_menu = self.menu_bar.addMenu("文件")
         self.open_mod_list_action = self._add_action(
-            file_menu, "Open Mod List…", "Ctrl+O"
+            file_menu, "导入模组排序…", "Ctrl+O"
         )
         file_menu.addSeparator()
         self.save_mod_list_action = self._add_action(
-            file_menu, "Save Mod List As…", "Ctrl+Shift+S"
+            file_menu, "保存模组排序…", "Ctrl+Shift+S"
         )
         file_menu.addSeparator()
-        self.import_submenu = QMenu("Import")
+        self.import_submenu = QMenu("导入")
         file_menu.addMenu(self.import_submenu)
         self.import_from_rentry_action = self._add_action(
-            self.import_submenu, "From Rentry.co"
+            self.import_submenu, "从 Rentry.co"
         )
         self.import_from_workshop_collection_action = self._add_action(
-            self.import_submenu, "From Workshop collection"
+            self.import_submenu, "从创意工坊集合"
         )
-        self.export_submenu = QMenu("Export")
+        self.export_submenu = QMenu("导出")
         file_menu.addMenu(self.export_submenu)
         self.export_to_clipboard_action = self._add_action(
-            self.export_submenu, "To Clipboard…"
+            self.export_submenu, "到剪贴板…"
         )
         self.export_to_rentry_action = self._add_action(
-            self.export_submenu, "To Rentry.co…"
+            self.export_submenu, "到 Rentry.co…"
         )
         file_menu.addSeparator()
-        self.upload_submenu = QMenu("Upload Log")
+        self.upload_submenu = QMenu("上传日志")
         file_menu.addMenu(self.upload_submenu)
         self.upload_rimsort_log_action = self._add_action(
             self.upload_submenu, "RimSort.log"
@@ -139,25 +139,25 @@ class MenuBar(QObject):
             self.upload_submenu, "RimWorld Player.log"
         )
         file_menu.addSeparator()
-        self.shortcuts_submenu = QMenu("Shortcuts")
+        self.shortcuts_submenu = QMenu("快捷方式")
         file_menu.addMenu(self.shortcuts_submenu)
         self.open_app_directory_action = self._add_action(
-            self.shortcuts_submenu, "Open RimSort Directory"
+            self.shortcuts_submenu, "打开 RimSort 目录"
         )
         self.open_settings_directory_action = self._add_action(
-            self.shortcuts_submenu, "Open RimSort User Files"
+            self.shortcuts_submenu, "打开 RimSort 用户文件"
         )
         self.open_rimsort_logs_directory_action = self._add_action(
-            self.shortcuts_submenu, "Open RimSort Logs Directory"
+            self.shortcuts_submenu, "打开 RimSort 日志目录"
         )
         self.open_rimworld_logs_directory_action = self._add_action(
-            self.shortcuts_submenu, "Open RimWorld Logs Directory"
+            self.shortcuts_submenu, "打开 RimWorld 日志目录"
         )
         if SystemInfo().operating_system != SystemInfo.OperatingSystem.MACOS:
             file_menu.addSeparator()
-            self.settings_action = self._add_action(file_menu, "Settings…", "Ctrl+,")
+            self.settings_action = self._add_action(file_menu, "设置…", "Ctrl+,")
             file_menu.addSeparator()
-            self.quit_action = self._add_action(file_menu, "Exit", "Ctrl+Q")
+            self.quit_action = self._add_action(file_menu, "退出", "Ctrl+Q")
         return file_menu
 
     def _create_edit_menu(self) -> QMenu:
@@ -167,12 +167,12 @@ class MenuBar(QObject):
         Returns:
             QMenu: The created "Edit" menu.
         """
-        edit_menu = self.menu_bar.addMenu("Edit")
-        self.cut_action = self._add_action(edit_menu, "Cut", "Ctrl+X")
-        self.copy_action = self._add_action(edit_menu, "Copy", "Ctrl+C")
-        self.paste_action = self._add_action(edit_menu, "Paste", "Ctrl+V")
+        edit_menu = self.menu_bar.addMenu("编辑")
+        self.cut_action = self._add_action(edit_menu, "剪切", "Ctrl+X")
+        self.copy_action = self._add_action(edit_menu, "复制", "Ctrl+C")
+        self.paste_action = self._add_action(edit_menu, "粘贴", "Ctrl+V")
         edit_menu.addSeparator()
-        self.rule_editor_action = self._add_action(edit_menu, "Rule Editor…")
+        self.rule_editor_action = self._add_action(edit_menu, "规则编辑器…")
         return edit_menu
 
     def _create_download_menu(self) -> QMenu:
@@ -182,12 +182,12 @@ class MenuBar(QObject):
         Returns:
             QMenu: The created "Download" menu.
         """
-        download_menu = self.menu_bar.addMenu("Download")
-        self.add_git_mod_action = self._add_action(download_menu, "Add Git Mod")
+        download_menu = self.menu_bar.addMenu("下载")
+        self.add_git_mod_action = self._add_action(download_menu, "添加 Git 模组")
         download_menu.addSeparator()
-        self.browse_workshop_action = self._add_action(download_menu, "Browse Workshop")
+        self.browse_workshop_action = self._add_action(download_menu, "浏览创意工坊")
         self.update_workshop_mods_action = self._add_action(
-            download_menu, "Update Workshop Mods"
+            download_menu, "更新创意工坊模组"
         )
         return download_menu
 
@@ -198,23 +198,23 @@ class MenuBar(QObject):
         Returns:
             QMenu: The created "Instances" menu.
         """
-        instances_menu = self.menu_bar.addMenu("Instances")
-        self.instances_submenu = QMenu('Current: "Default"')
+        instances_menu = self.menu_bar.addMenu("实例")
+        self.instances_submenu = QMenu('当前: "默认"')
         instances_menu.addMenu(self.instances_submenu)
         instances_menu.addSeparator()
         self.backup_instance_action = self._add_action(
-            instances_menu, "Backup Instance…"
+            instances_menu, "备份实例…"
         )
         self.restore_instance_action = self._add_action(
-            instances_menu, "Restore Instance…"
+            instances_menu, "恢复实例…"
         )
         instances_menu.addSeparator()
-        self.clone_instance_action = self._add_action(instances_menu, "Clone Instance…")
+        self.clone_instance_action = self._add_action(instances_menu, "克隆实例…")
         self.create_instance_action = self._add_action(
-            instances_menu, "Create Instance…"
+            instances_menu, "创建实例…"
         )
         self.delete_instance_action = self._add_action(
-            instances_menu, "Delete Instance…"
+            instances_menu, "删除实例…"
         )
         return instances_menu
 
@@ -225,13 +225,13 @@ class MenuBar(QObject):
         Returns:
             QMenu: The created "Textures" menu.
         """
-        texture_menu = self.menu_bar.addMenu("Textures")
+        texture_menu = self.menu_bar.addMenu("纹理/贴图")
         self.optimize_textures_action = self._add_action(
-            texture_menu, "Optimize Textures"
+            texture_menu, "优化纹理/贴图"
         )
         texture_menu.addSeparator()
         self.delete_dds_textures_action = self._add_action(
-            texture_menu, "Delete .dds Textures"
+            texture_menu, "删除.dds纹理/贴图"
         )
         return texture_menu
 
@@ -242,7 +242,7 @@ class MenuBar(QObject):
         Returns:
             QMenu: The created "Help" menu.
         """
-        help_menu = self.menu_bar.addMenu("Help")
+        help_menu = self.menu_bar.addMenu("帮助")
         self.wiki_action = self._add_action(help_menu, "RimSort Wiki…")
         help_menu.addSeparator()
         # TODO: updates not implemented yet
@@ -254,7 +254,7 @@ class MenuBar(QObject):
         # )
         # help_menu.addSeparator()
         self.validate_steam_client_action = self._add_action(
-            help_menu, "Validate Steam Client mods"
+            help_menu, "验证 Steam 客户端模组"
         )
         return help_menu
 
@@ -263,16 +263,16 @@ class MenuBar(QObject):
         Create the menu bar. On macOS, include the app menu.
         """
         if SystemInfo().operating_system == SystemInfo.OperatingSystem.MACOS:
-            app_menu = self.menu_bar.addMenu("AppName")
+            app_menu = self.menu_bar.addMenu("应用名称")
             app_menu.addSeparator()
             self.settings_action = self._add_action(
                 app_menu,
-                "Settings...",
+                "设置...",
                 shortcut="Ctrl+,",
                 role=QAction.MenuRole.ApplicationSpecificRole,
             )
             app_menu.addSeparator()
-            self.quit_action = self._add_action(app_menu, "Quit")
+            self.quit_action = self._add_action(app_menu, "退出")
         self._create_file_menu()
         self._create_edit_menu()
         self._create_download_menu()
